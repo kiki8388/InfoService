0f850ca7ddb7b599a76872ac6835db83453e738c
kiki8388 <mikoxx@outlook.com>
2024-12-07 13:44:44 +0100

    update CreateComment command

--- Backend/InfoServiceAPI/Models/Commands/CreateComment.cs
+++ Backend/InfoServiceAPI/Models/Commands/CreateComment.cs
@@ -1,9 +1,13 @@
-﻿namespace InfoServiceAPI.Models.Commands
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InfoServiceAPI.Models.Commands
 {
     public class CreateComment
     {
         public Guid PostId { get; set; }
+        [Required]
         public string Author { get; set; }
+        [StringLength(2000)]
         public string Content { get; set; }
     }
 }
